0d4aef894bbf170075ed8357aa38de6528f0db0a
LE4ndr01 <101906625+LE4ndr01@users.noreply.github.com>
2022-06-11 18:25:06 -0400

    creacion y configuracion de BD

--- web/core/admin.py
+++ web/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categoria,Usuario
 
 # Register your models here.
+# Administrar modelo
+
+admin.site.register(Categoria)
+admin.site.register(Usuario)
--- web/core/models.py
+++ web/core/models.py
@@ -1,3 +1,26 @@
+from msilib.schema import Class
 from django.db import models
 
 # Create your models here.
+
+#MODELO PARA CATEGORIAS
+
+class Categoria(models.Model):
+    idcategoria = models.AutoField(primary_key=True,verbose_name="Id Categoria")
+    nombreCategoria = models.CharField(max_length=50,verbose_name="Nombre Categoria")
+        
+    def __str__(self):
+        return self.nombreCategoria
+        
+#MODELO PARA USUARIOS
+
+class Usuario(models.Model):
+    idusuario = models.AutoField(primary_key=True,verbose_name="Id Usuario")
+    nombreusuario = models.CharField(max_length=100,verbose_name="Nombre Usuario")
+    correousuario = models.CharField(max_length=200,verbose_name="Correo Usuario")
+    contrasenausuario = models.CharField(max_length=50,verbose_name="Contraseña Usuario")
+    Categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombreusuario
+    
