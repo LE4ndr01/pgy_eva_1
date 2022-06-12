a556a527dcbd934977f279537cb018b157d022c9
LE4ndr01 <101906625+LE4ndr01@users.noreply.github.com>
2022-06-12 02:16:21 -0400

    redireccionamiento con django

--- web/core/urls.py
+++ web/core/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import home
+from .views import inicio, nosotros, contacto, venta, seguimiento, dashuser, ordenes, formulario, crear
 
 
 urlpatterns = [
-    path('', home, name= "home"),
+    path('', inicio, name= "inicio"),
+    path('nosotros', nosotros, name= "nosotros"),
+    path('contacto', contacto, name= "contacto"),
+    path('venta', venta, name= "venta"),
+    path('seguimiento', seguimiento, name= "seguimiento"),
+    path('dashuser', dashuser, name= "dashuser"),
+    path('ordenes', ordenes, name= "ordenes"),
+    path('formulario', formulario, name= "formulario"),
+    path('crear', crear, name= "crear"),
 ]
--- web/core/views.py
+++ web/core/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
 
+
 # Create your views here.
-def home (request):
-    return render(request, 'core/index.html' )
+def inicio (request):
+    return render(request, 'core/index.html')
+def nosotros (request):
+    return render(request, 'core/nosotros.html')
+def contacto (request):
+    return render(request, 'core/forcontacto.html')
+def venta (request):
+    return render(request, 'core/ventasproductos.html')
+def seguimiento (request):
+    return render(request, 'core/seguimiento.html')
+def dashuser (request):
+    return render(request, 'core/dashuser.html')
+def ordenes (request):
+    return render(request, 'core/ordenes.html')
+def formulario (request):
+    return render(request, 'core/formulario.html')
+def crear (request):
+    return render(request, 'core/ccuenta.html')
